f151ce819da7c6e7bd814c53cf5c24b049878a4a
John Keenan <jgknomad@gmail.com>
2023-11-29 03:55:36 -0500

    Create and enqueue zenStyle.css with test selector.
    This will give all main elements a forced pink background, this is purely for testing purposes to make it easier to identify when the style had been setup successfully.

--- themes/everydayZen-child/functions.php
+++ themes/everydayZen-child/functions.php
@@ -7,6 +7,11 @@ function everyday_zen_enqueue_styles(){
         array("twenty-twenty-one-style"),
         wp_get_theme()->get("version")
     );
+
+    wp_enqueue_style(
+        "zenStyle",
+        get_stylesheet_directory_uri() . "/zenStyle.css"
+    );
 }
 
 add_action("wp_enqueue_scripts", "everyday_zen_enqueue_styles");
